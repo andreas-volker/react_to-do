f636d19b64cd19cab2cda33952f5e7b83d6e0e98
Andreas Völker <andreaskvolker@gmail.com>
2020-04-02 12:17:12 -0300

    deploy

---
+++ build/precache-manifest.7ae6d8053df8fdb78547233dc15e45dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfab0b210c69afb617410048e33572a",
+    "url": "/react_to-do/index.html"
+  },
+  {
+    "revision": "03ef97adb2e4823e8011",
+    "url": "/react_to-do/static/css/main.ee31cd3c.chunk.css"
+  },
+  {
+    "revision": "bcbccb7e0383a71c9a7b",
+    "url": "/react_to-do/static/js/2.0e33b1f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_to-do/static/js/2.0e33b1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ef97adb2e4823e8011",
+    "url": "/react_to-do/static/js/main.a4e8caec.chunk.js"
+  },
+  {
+    "revision": "5c718262af4767c663b3",
+    "url": "/react_to-do/static/js/runtime-main.1bf702ac.js"
+  }
+]);
